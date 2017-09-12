b417554a33e10b397521debd1a3a85389c0d585b
psychofarm <psychofarm@me.com>
2017-09-12 06:05:47 -0400

    v0.0.2

--- Week2/pap_result/static/js/apps.js
+++ Week2/pap_result/static/js/apps.js
@@ -2,6 +2,19 @@
 
 $(document).ready(function(){
     $( function() {
-        // any formatting code will go here
+        console.log("in listener");
+        "use strict";
+        window.addEventListener("load", function() {
+            var form = document.getElementById("needs-validation");
+            form.addEventListener("submit", function(event) {
+                if (form.checkValidity() == false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+            }, false);
+        }, false);
     });
 });
+
+// validity is not working
